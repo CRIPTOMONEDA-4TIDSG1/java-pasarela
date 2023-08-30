0f4198142faf7f588d88fe1a3f454b9e972e044b
kalet chavez <kalet.svdev@gmail.com>
2023-08-30 11:04:18 -0600

    arreglo de problema

--- CryptoWeb/src/main/java/Controllers/ProductController.java
+++ CryptoWeb/src/main/java/Controllers/ProductController.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author kalet
  */
+@WebServlet(name="ProductController", urlPatterns= {"/ProductController"})
 public class ProductController extends HttpServlet {
 
     /**
